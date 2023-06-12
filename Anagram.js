2494491b626f778fc9092a42eb45fcc65dd1175d
anandhu-as <anandhuas010203@gmail.com>
2023-06-12 21:26:16 +0530

    Valid Anagram

---
+++ Anagram.js
@@ -0,0 +1,9 @@
+{
+  /* Valid Anagram*/
+}
+
+const isAnagram = (s, t)=> {
+    const string1=s.split("").sort().join();
+    const string2=t.split('').sort().join();
+    return string==string2
+};
